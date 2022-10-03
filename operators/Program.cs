c49fb92473bbaf5198e4eb21aa1f7ca2ba6360aa
Darwin Quiñones <darwinquinones735@gmail.com>
2022-10-02 20:48:48 -0500

    Se completa el Vídeo del tema 2: Variables, Tipos, Operadores

---
+++ operators/Program.cs
@@ -0,0 +1,76 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+// operadores aritmeticos
+
+/* suma  +
+ * resta - 
+ * multiplicacion *
+ * divicion /
+ * modulo %
+ * auto incremento ++
+ * auto decremento --
+ */
+
+int i = 7;
+Console.WriteLine("modulo "+ i%2);
+int t = 0;
+Console.WriteLine($"autoincremento {++t}");
+Console.WriteLine($"autodecremento {--t}");
+Console.WriteLine($"Entero {t}");
+
+// operadores de asignacion
+
+/* * asignacion =
+ * https://www.w3schools.com/cs/cs_operators_assignment.php
+ * 
+ * 
+ * !
+ */
+int j = 10;
+int p = 2;
+
+//Console.WriteLine("Suma " + (j += 2));
+//Console.WriteLine("resta " + (j -= 2));
+//Console.WriteLine("multiplicacion " + (j *= 2));
+//Console.WriteLine("division " + (j /= 2));
+//Console.WriteLine("modulo " + (j %= 2));
+
+
+/* operadores de comparacion o relacionales
+Console.WriteLine($"{j} es igual a {p} : {j == p}");
+Console.WriteLine($"{j} es menor que {p} : {j < p}");
+Console.WriteLine($"{j} es mayor que {p} : {j > p}");
+Console.WriteLine($"{j} es mayor o igual que {p} : {j >= p}");
+*/
+
+// operadores logicos
+
+/**
+ * logico y && 
+ * logico o || 
+ * logico no !   	!(x < 5 && x < 10) Invierte el resultado, devuelve False si el resultado es verdadero
+ */
+
+int a = 9;
+int b = 11;
+
+Console.WriteLine("AND - Y");
+Console.WriteLine("True True " + (a < 10 && b > 10));
+Console.WriteLine("True False " + (a < 10 && b < 10));
+Console.WriteLine("False True " + (a > 10 && b > 10));
+Console.WriteLine("False False " + (a > 10 && b < 10));
+
+Console.WriteLine("OR - O");
+Console.WriteLine("True True " + (a < 10 || b > 10));
+Console.WriteLine("True False " + (a < 10 || b < 10));
+Console.WriteLine("False True " + (a > 10 || b > 10));
+Console.WriteLine("False False " + (a > 10 ||  b < 10));
+
+Console.WriteLine("NOT - NO");
+Console.WriteLine("True " + !(a < 10));
+Console.WriteLine("False " + !(a > 10));
+
+
+
---
+++ variablesOperadores/Program.cs
@@ -0,0 +1,61 @@
+﻿/**
+ * 
+ tipos de datos
+*/
+int i = 1;
+float miFloat = i;
+long l = 23455;
+
+float f = 2.3456f;
+double d = 3.55555555d;
+
+char c = 'C';
+string s = "string";
+
+
+
+
+//Console.Write($"int: {i}");
+//Console.WriteLine($" long: {l}");
+
+//Console.Write($"float: {f}");
+//Console.WriteLine($" double: {d}");
+
+//Console.Write($"char: {c}");
+//Console.WriteLine($" string: {s}");
+
+//Console.WriteLine($"miFloat {miFloat}");
+
+
+//// datos de persona
+//// name
+//Console.WriteLine("Write your name: ");
+//string name = Console.ReadLine();
+
+////Edad
+//Console.WriteLine("Introduce your age: ");
+//string age = Console.ReadLine();
+
+////Ciudad
+//Console.WriteLine("Introduce your City: ");
+//string city = Console.ReadLine();
+
+//Console.WriteLine($"Helo {name}, your age is {age} and you live in {city}");
+
+
+
+// constantes
+const float pi = 3.1415f;
+float cualquierFloat = pi + 3;
+//Console.WriteLine($"al cuadrado {Math.Pow(i, 2)}");
+
+/**
+ * nullables- tener en cuenta
+ c# es un lenguaje fuertemente tipado esto significa que las variables 
+tienen un unico tipo de datos
+ */
+
+
+//int? t = 0;
+//Console.WriteLine(t.HasValue);
+//Console.WriteLine( t + 2);
